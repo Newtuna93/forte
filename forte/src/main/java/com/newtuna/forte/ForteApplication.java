8da3ab3959f25a2ea9b7e53b98a0b6021e2ca8b3
신동원 <ehddnjs494949@gmail.com>
2023-08-16 15:20:18 +0900

    new Project Created!

---
+++ forte/src/main/java/com/newtuna/forte/ForteApplication.java
@@ -0,0 +1,13 @@
+package com.newtuna.forte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForteApplication.class, args);
+	}
+
+}
