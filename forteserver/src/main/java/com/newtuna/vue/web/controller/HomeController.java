932646b160d45c36e15f8bfe5af65b4cb3707492
신동원 <ehddnjs494949@gmail.com>
2023-08-16 16:49:01 +0900

    java server project setting complete

--- forteserver/src/main/java/com/newtuna/vue/web/controller/HomeController.java
+++ forteserver/src/main/java/com/newtuna/vue/web/controller/HomeController.java
@@ -1,8 +1,13 @@
 package com.newtuna.vue.web.controller;
 
-import org.spring
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@Contorller
+@Controller
 public class HomeController {
 
+	@GetMapping("/")
+	public String index() {
+		return "index.html";
+	}
 }
